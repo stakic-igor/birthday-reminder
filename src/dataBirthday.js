c1c065c6cc1cbd63cf20ede0db7e9185985bd169
Igor Stakic <stakic@mdpi.com>
2020-11-13 17:40:41 +0100

    improve data for app

--- src/dataBirthday.js
+++ src/dataBirthday.js
@@ -2,25 +2,55 @@ export const dataBirthday = [
     {
         id: 1,
         name: 'John Doe',
-        role: 'Developer',
+        years: 29,
         userAvatarUrl: 'https://randomuser.me/api/portraits/men/84.jpg'
     },
     {
         id: 2,
         name: 'Jessica Doe',
-        role: 'Designer',
+        years: 31,
         userAvatarUrl: 'https://randomuser.me/api/portraits/women/82.jpg'
     },
     {
         id: 3,
         name: 'Megan Fox',
-        role: 'Project Manager',
+        years: 32,
         userAvatarUrl: 'https://randomuser.me/api/portraits/women/88.jpg'
     },
     {
         id: 4,
         name: 'Stephen Wolf',
-        role: 'QA',
+        years: 25,
         userAvatarUrl: 'https://randomuser.me/api/portraits/men/94.jpg'
+    },
+    {
+        id: 5,
+        name: 'Tony Flow',
+        years: 40,
+        userAvatarUrl: 'https://randomuser.me/api/portraits/men/44.jpg'
+    },
+    {
+        id: 6,
+        name: 'Steven Taylor',
+        years: 23,
+        userAvatarUrl: 'https://randomuser.me/api/portraits/men/11.jpg'
+    },
+    {
+        id: 7,
+        name: 'Ann Wolfenstain',
+        years: 23,
+        userAvatarUrl: 'https://randomuser.me/api/portraits/women/72.jpg'
+    },
+    {
+        id: 8,
+        name: 'Mel Hawkings',
+        years: 33,
+        userAvatarUrl: 'https://randomuser.me/api/portraits/women/69.jpg'
+    },
+    {
+        id: 9,
+        name: 'Joseh Drinkwater',
+        years: 52,
+        userAvatarUrl: 'https://randomuser.me/api/portraits/men/10.jpg'
     },
 ]
