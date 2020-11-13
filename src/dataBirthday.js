c4e7fd515ef1b76924c21d79f7c405f3ce90f8cb
Igor Stakic <stakic@mdpi.com>
2020-11-13 17:07:44 +0100

    Add new data and user images

---
+++ src/dataBirthday.js
@@ -0,0 +1,26 @@
+export const dataBirthday = [
+    {
+        id: 1,
+        name: 'John Doe',
+        role: 'Developer',
+        userAvatarUrl: 'https://randomuser.me/api/portraits/men/84.jpg'
+    },
+    {
+        id: 2,
+        name: 'Jessica Doe',
+        role: 'Designer',
+        userAvatarUrl: 'https://randomuser.me/api/portraits/women/82.jpg'
+    },
+    {
+        id: 3,
+        name: 'Megan Fox',
+        role: 'Project Manager',
+        userAvatarUrl: 'https://randomuser.me/api/portraits/women/88.jpg'
+    },
+    {
+        id: 4,
+        name: 'Stephen Wolf',
+        role: 'QA',
+        userAvatarUrl: 'https://randomuser.me/api/portraits/men/94.jpg'
+    },
+]
